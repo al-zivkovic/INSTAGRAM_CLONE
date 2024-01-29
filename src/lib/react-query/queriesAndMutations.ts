56525d1e24e864d5e9c1968683d8a169c9089cfd
al-zivkovic <91557608+al-zivkovic@users.noreply.github.com>
2024-01-28 16:57:15 -0800

    Create posts
    Functionality to create posts and upload files to the database

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -3,15 +3,17 @@ import {
  useMutation,
  useQueryClient,
  useInfiniteQuery,
-} from '@tanstack/react-query'
-import { createUserAccount, signInAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+} from '@tanstack/react-query';
+
+import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
+import { INewPost, INewUser } from '@/types';
+import { QUERY_KEYS } from './queryKeys';
 
 export const useCreateUserAccount = () => {
     return useMutation({
         mutationFn: (user: INewUser) => createUserAccount(user)
-    })
-}
+    });
+};
 
 export const useSignInAccount = () => {
     return useMutation({
@@ -19,5 +21,24 @@ export const useSignInAccount = () => {
             email: string, 
             password: string
         }) => signInAccount(user)
-    })
-}
+    });
+};
+
+export const useSignOutAccount = () => {
+    return useMutation({
+        mutationFn: signOutAccount
+    });
+};
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (post: INewPost) => createPost(post),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+      },
+    });
+};
+  
